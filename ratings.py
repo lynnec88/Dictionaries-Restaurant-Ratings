10988e3037bcd546131739c63e87c5bc8614db60
陈玲琳 <chenlinglin@chenlinglindeMacBook-Pro.local>
2023-03-20 19:32:45 -0400

    revised

--- ratings.py
+++ ratings.py
@@ -4,6 +4,8 @@
 # put your code here
 
 def process_scores():
+    """Read scores file and return dictionary of {restaurant-name: score}."""
+
     scores_txt= open('scores.txt')
     scores= {}
     for line in scores_txt:
@@ -14,6 +16,8 @@ def process_scores():
     return scores
 
 def add_restaurant(scores):
+    """Add a restaurant and rating."""
+    
     print("please add your favorite restaurant and rating for them!")
     restaurant =  input ("restaurant name:")
     rating= int(input("rating:"))
@@ -21,11 +25,16 @@ def add_restaurant(scores):
     scores[restaurant] = rating
 
 def print_sorted_scores(scores):
+    """Print restaurants and ratings, sorted."""
 # Print all of the ratings in alphabetical order (including the new one, of course)
     for restaurant , rating in sorted(scores.items()):
         print(f"{restaurant} is rated at {rating}.")
 
-
+# read existing scores in from scores.txt file
 scores = process_scores()
+
+# allow user to add a restaurant/rating pair
 add_restaurant(scores)
+
+# print an alphabetical list of all rated restaurants and their ratings
 print_sorted_scores(scores)
